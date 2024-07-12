bd105903221bf6a07a8c28ea56aea00d4416f43d
brwei01 <bwei01@mail.bbk.ac.uk>
2024-07-12 20:07:38 +0100

    added more annotation

--- SeleniumDemo.py
+++ SeleniumDemo.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
-# setup chrome options for downloads
+# Setup chrome options for downloads
 chrome_options = Options()
 chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--disable-popup-blocking")
@@ -22,19 +22,19 @@ chrome_options.add_experimental_option('prefs',
                                            "plugins.always_open_pdf_externally": True
                                        })
 
-# set up the webdriver
+# Set up the webdriver
 service = Service(r'/Users/apple/Dev/tools/chromedriver-mac-x64/chromedriver')
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# try to click the search btn
+# Try to click the search btn
 try:
     # open target url
     url = "https://www1.hkexnews.hk/search/titlesearch.xhtml?lang=en"
     driver.get(url)
 
-    # search pattern for PDF files
+    # Search pattern for PDF files
     pdf_pattern = re.compile(r'\.pdf$', re.IGNORECASE)
-    # search patters for ESG and env-related docs
+    # Search patters for ESG and env-related docs
     esg_pattern = re.compile(r'\b(ESG|environment|environmental)\b', re.IGNORECASE)
 
     search_btn = driver.find_element(By.CLASS_NAME, 'filter__btn-applyFilters-js')
@@ -102,7 +102,7 @@ try:
     except NoSuchElementException as e:
         print(f"An error occurred: {e}")
 
-    # Adjust the locator method as necessary (e.g., By.XPATH, By.CLASS_NAME, etc.)
+    # Find links to documents
     row_elements = driver.find_elements(By.CLASS_NAME, "doc-link")
     for row_element in row_elements:
         try:
@@ -112,7 +112,7 @@ try:
             if esg_pattern.search(link_element.text):
                 print(f"Found ESG related pdf doc: {href}!")
                 try:
-                    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.TAG_NAME,"a")))
+                    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.TAG_NAME, "a")))
                     # link_element.click()
                     # time.sleep(1)
                 except NoSuchElementException:
@@ -121,7 +121,7 @@ try:
                     continue
 
         except NoSuchElementException:
-            print("Anchor tag not found in this row")
+            print("Anchor tag (doc-link) not found in this row")
 
 finally:
     # Close the browser
